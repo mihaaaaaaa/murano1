79c7bf02b694735f5f6b35562fa21e8cb03b4c23
mihaaaaaaa <mihakruts@yandex.ru>
2018-05-19 19:27:57 +0400

    1

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	let total = 
 	$('.spisok-3-item-br').click(function(){
 	let value = '.value' // создаём переменную для вычесленний
 	valve = valve.valve + 1 //добавляем к переменной "valve" 1
