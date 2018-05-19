66b2244b6afbb388921af221769f3bdfdcb18da9
mihaaaaaaa <mihakruts@yandex.ru>
2018-05-19 19:17:37 +0400

    3

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,2 +1,6 @@
 $(document).ready(function() {
+	$('.spisok-3-item-br').click(function(){
+	let value = '.value' // создаём переменную для вычесленний
+	valve = valve.valve + 1 //добавляем к переменной "valve" 1
+	})
 });
