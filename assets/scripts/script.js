99ed39aa7edf051a3f694a831eb0faa146624198
mihaaaaaaa <mihakruts@yandex.ru>
2018-05-19 18:56:38 +0400

    2
    2

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,12 +1,2 @@
-	function addTextToInput(anElement) {
-  var text = document.getElementById('jaloby').value;
-  text += 1;
-  document.getElementById('jaloby').value = text;
-}
-
 $(document).ready(function() {
-    let total = 0;
-    $('.spisok-3-item-bl').click(function () {
-        $('.spisok-3-item-input').valve(total++)
-    });
 });
