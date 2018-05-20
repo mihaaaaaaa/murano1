7429374e298e5f83ff4f58f64e202884e25f9e58
mihaaaaaaa <mihakruts@yandex.ru>
2018-05-20 22:31:01 +0400

    5
    кнопки работаюююют

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,29 @@
 $(document).ready(function() {
-	let total = 
 	$('.spisok-3-item-br').click(function(){
-	let value = '.value' // создаём переменную для вычесленний
-	valve = valve.valve + 1 //добавляем к переменной "valve" 1
+	var x = document.getElementById("value").value; // создаём переменную для вычесленний
+	x++; //добавляем к переменной "X" 1
+	if (x > 10) {
+		x = 10 ;
+	}
+	document.getElementById("value").value = x;
+	var y = document.getElementById('total').value;
+	y = +11000;
+	document.getElementById("total").innerHTML = y;
 	})
+
+	$('.spisok-3-item-bl').click(function(){
+	var x = document.getElementById("value").value; // создаём переменную для вычесленний
+	x--; //отнимаем от переменной "X" 1
+	if (x = 0) {
+		x = 0 ;
+	}
+	document.getElementById("value").value = x;
+	var y = document.getElementById('total').value;
+	y = -11000;
+	document.getElementById("total").innerHTML = y;
+	})
+
+	if (total > 10000) {
+		document.getElementById("total").innerHTML = "5" ; //если "total" превышает 10000 то скидка = 5%
+	}
 });
